8ad75310d9b0641edfc91146b1c22bb17db0f31e
Julie Cordeiro <juliecordeiro1@gmail.com>
2023-10-20 15:56:21 -0300

    feat: subindo projeto

---
+++ src/main/java/dio/iphone/IPhone.java
@@ -0,0 +1,49 @@
+package dio.iphone;
+
+public class IPhone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet {
+
+    @Override
+    public void tocar() {
+        System.out.println("Reproduzindo música");
+    }
+
+    @Override
+    public void pausar() {
+        System.out.println("Pausando música");
+    }
+
+    @Override
+    public void selecionarMusica(String musica) {
+        System.out.println("Selecionando música: " + musica);
+    }
+
+    @Override
+    public void ligar() {
+        System.out.println("Ligando o telefone");
+    }
+
+    @Override
+    public void atender() {
+        System.out.println("Atendendo chamada");
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+        System.out.println("Iniciando correio de voz");
+    }
+
+    @Override
+    public void exibirPagina(String url) {
+        System.out.println("Exibindo página: " + url);
+    }
+
+    @Override
+    public void adicionarNovaAba(String url) {
+        System.out.println("Adicionando nova aba com a página: " + url);
+    }
+
+    @Override
+    public void atualizarPagina() {
+        System.out.println("Atualizando página");
+    }
+}
---
+++ src/main/java/dio/iphone/ReprodutorMusical.java
@@ -0,0 +1,21 @@
+package dio.iphone;
+
+interface ReprodutorMusical {
+    void tocar();
+    void pausar();
+    void selecionarMusica(String musica);
+}
+
+// Interface para Aparelho Telefônico
+interface AparelhoTelefonico {
+    void ligar();
+    void atender();
+    void iniciarCorreioVoz();
+}
+
+// Interface para Navegador na Internet
+interface NavegadorInternet {
+    void exibirPagina(String url);
+    void adicionarNovaAba(String url);
+    void atualizarPagina();
+}
